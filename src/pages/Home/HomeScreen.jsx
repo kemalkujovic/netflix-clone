44bf7a96854123759c517de23111dd7572999e86
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-12 19:18:51 +0200

    add baner && style

--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "../../components/Nav/Nav";
+import Banner from "../../components/Banner/Banner";
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
-      {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
