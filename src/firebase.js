43c440b794ca2be94eb65b0ef462444fe8cf3537
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-12 18:34:44 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXuetAfoQdn-yzvcFAAsqWx2tvfNye9bU",
+  authDomain: "netflix-clone-46f1f.firebaseapp.com",
+  projectId: "netflix-clone-46f1f",
+  storageBucket: "netflix-clone-46f1f.appspot.com",
+  messagingSenderId: "174481017338",
+  appId: "1:174481017338:web:14402cf001fdd443397c43",
+};
