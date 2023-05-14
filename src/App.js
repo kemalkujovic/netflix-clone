1434869a4d77fc7ae2e003a275afc150e54e69e4
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-14 18:03:00 +0200

    add row component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HomeScreen from "./pages/Home/HomeScreen";
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -2,13 +2,45 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "../../components/Nav/Nav";
 import Banner from "../../components/Banner/Banner";
+import request from "../../axios/Request";
+import Row from "../../components/Row/Row";
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
 
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} isLargeRow />
+      <Row
+        title="Action Movies"
+        fetchUrl={request.fetchActionMovies}
+        isLargeRow
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={request.fetchComedyMovies}
+        isLargeRow
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={request.fetchHorrorMovies}
+        isLargeRow
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={request.fetchRomanceMovies}
+        isLargeRow
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={request.fetchDocumentaries}
+        isLargeRow
+      />
     </div>
   );
 };
