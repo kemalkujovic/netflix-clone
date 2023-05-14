ae6ff14511aa5c4a4a4bf862924ecbfea71efe54
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-14 18:17:06 +0200

    fetch movies

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Row.css";
-const Row = () => {
-  return <div className="row"></div>;
+import axios from "../../axios/axios";
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+  const [movies, setMovies] = useState([]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      {movies.map((movie) => (
+        <img
+          className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+          src={`${base_url}${
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Row;
--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -16,31 +16,11 @@ const HomeScreen = () => {
         isLargeRow
       />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} isLargeRow />
-      <Row
-        title="Action Movies"
-        fetchUrl={request.fetchActionMovies}
-        isLargeRow
-      />
-      <Row
-        title="Comedy Movies"
-        fetchUrl={request.fetchComedyMovies}
-        isLargeRow
-      />
-      <Row
-        title="Horror Movies"
-        fetchUrl={request.fetchHorrorMovies}
-        isLargeRow
-      />
-      <Row
-        title="Romance Movies"
-        fetchUrl={request.fetchRomanceMovies}
-        isLargeRow
-      />
-      <Row
-        title="Documentaries"
-        fetchUrl={request.fetchDocumentaries}
-        isLargeRow
-      />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 };
