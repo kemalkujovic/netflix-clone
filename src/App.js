d8b8a0e84f536f456e40d1302339f70fea468e31
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-14 19:58:46 +0200

    add login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import HomeScreen from "./pages/Home/HomeScreen";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
 function App() {
+  const user = null;
+
   return (
     <div className="App">
-      <HomeScreen />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            {!user ? (
+              <Route path="/login" element={<LoginPage />} />
+            ) : (
+              <Route index element={<HomeScreen />} />
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
