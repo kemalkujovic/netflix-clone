f969059ef9f9347e1f9665a70183eba9ffdf6b5a
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-30 18:35:58 +0200

    style signup screen

--- src/pages/Signup/SignupScreen.jsx
+++ src/pages/Signup/SignupScreen.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./SignupScreen.css";
 const SignupScreen = () => {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
-
-        <h4>New to Netflix? Sign Up now.</h4>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
   );
