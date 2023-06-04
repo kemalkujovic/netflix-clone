a459ddf6a71d87c0acf3031e535d58eb90c06e32
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-06-04 17:22:50 +0200

    fix request urls

--- src/axios/Request.js
+++ src/axios/Request.js
@@ -5,11 +5,11 @@ const request = {
   fetchTrending: `${baseURL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `${baseURL}/discover/tv?api_key=${API_KEY}&with_netowrks=213`,
   fetchTopRated: `${baseURL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&WITH_GENRES=28`,
-  fetchComedyMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&WITH_GENRES=35`,
-  fetchHorrorMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&WITH_GENRES=27`,
-  fetchRomanceMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&WITH_GENRES=10749`,
-  fetchDocumentaries: `${baseURL}/discover/movie?api_key=${API_KEY}&WITH_GENRES=99`,
+  fetchActionMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `${baseURL}/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default request;
