1ff61f57159dd6753bc10bdaf9b0ce5f7e66c320
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-06-07 10:13:19 +0200

    skeleton use Material Mui

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -2,41 +2,61 @@ import React, { useEffect, useState } from "react";
 import "./Row.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getFilms } from "../../app/filmsSlice";
+import { Skeleton } from "@mui/material";
+import Box from "@mui/material/Box";
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const base_url = "https://image.tmdb.org/t/p/original/";
   const dispatch = useDispatch();
   const select = useSelector((state) => state.films);
   useEffect(() => {
     async function fetchData() {
       const request = await dispatch(getFilms(fetchUrl));
-      setMovies(request.payload.results);
+      await setMovies(request.payload.results);
       return request;
     }
 
     fetchData();
   }, [fetchUrl]);
 
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1500);
   return (
-    <div className="row">
-      <h2>{title}</h2>
-      <div className="row__posters">
-        {movies.map(
-          (movie) =>
-            ((isLargeRow && movie.poster_path) ||
-              (!isLargeRow && movie.backdrop_path)) && (
-              <img
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                key={movie.id}
-                src={`${base_url}${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie.name}
-              />
+    <>
+      <div className="row">
+        <h2>{title}</h2>
+        <div className="row__posters">
+          {movies.map((movie) =>
+            isLoading ? (
+              <Box sx={{ width: 300 }}>
+                <Skeleton
+                  sx={{ bgcolor: "grey.900" }}
+                  animation="wave"
+                  variant="rectangular"
+                  width={150}
+                  height={100}
+                  style={{ marginLeft: 20 }}
+                />
+              </Box>
+            ) : (
+              ((isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path)) && (
+                <img
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  key={movie.id}
+                  src={`${base_url}${
+                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                  }`}
+                  alt={movie.name}
+                />
+              )
             )
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -4,24 +4,39 @@ import Nav from "../../components/Nav/Nav";
 import Banner from "../../components/Banner/Banner";
 import request from "../../axios/Request";
 import Row from "../../components/Row/Row";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+
 const HomeScreen = () => {
+  const isLoading = true;
+  const rows = [
+    {
+      title: "NETFLIX ORIGINALS",
+      fetchUrl: request.fetchNetflixOriginals,
+      isLargeRow: true,
+    },
+    {
+      title: "Trending Now",
+      fetchUrl: request.fetchTrending,
+      isLargeRow: true,
+    },
+    { title: "Action Movies", fetchUrl: request.fetchActionMovies },
+    { title: "Comedy Movies", fetchUrl: request.fetchComedyMovies },
+    { title: "Horror Movies", fetchUrl: request.fetchHorrorMovies },
+    { title: "Romance Movies", fetchUrl: request.fetchRomanceMovies },
+    { title: "Documentaries", fetchUrl: request.fetchDocumentaries },
+  ];
+  console.log("render time");
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={request.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={request.fetchTrending} isLargeRow />
-      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+      {rows.map((row, index) => (
+        <Row
+          key={index}
+          title={row.title}
+          fetchUrl={row.fetchUrl}
+          isLargeRow={row.isLargeRow}
+        />
+      ))}
     </div>
   );
 };
