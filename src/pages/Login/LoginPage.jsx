46abe3558897803a82c9af6c7a31336c7a5deae1
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-06-17 03:18:31 +0200

    fix logo

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
         <img
           className="loginScreen__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-          alt=""
+          alt="img"
         />
         <button onClick={() => setSignIn(true)} className="loginScreen__button">
           Sign In
